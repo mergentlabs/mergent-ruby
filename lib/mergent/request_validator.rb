2c080b77afd75574035a5d362af99ee8d7b9ab6f
James Martinez <jamescmartinez@gmail.com>
2022-01-20 13:09:45 -0600

    Remove URL from RequestValidator (#17)

--- lib/mergent/request_validator.rb
+++ lib/mergent/request_validator.rb
@@ -6,14 +6,13 @@ module Mergent
       @api_key = api_key || Mergent.api_key
     end
 
-    def build_signature_for(url, body)
-      data = (url || "") + (body || "")
+    def build_signature_for(body)
       digest = OpenSSL::Digest.new("sha1")
-      Base64.strict_encode64(OpenSSL::HMAC.digest(digest, @api_key, data))
+      Base64.strict_encode64(OpenSSL::HMAC.digest(digest, @api_key, body || ""))
     end
 
-    def valid_signature?(url, body, signature)
-      build_signature_for(url, body) == signature
+    def valid_signature?(body, signature)
+      build_signature_for(body) == signature
     end
   end
 end
--- spec/mergent/request_validator_spec.rb
+++ spec/mergent/request_validator_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe Mergent::RequestValidator do
   subject(:validator) { described_class.new("12345") }
 
   describe "#build_signature_for" do
-    it "builds a HMAC-SHA1 signature for the provided url & body" do
-      url = "https://example.com/webhook"
+    it "builds a HMAC-SHA1 signature for the provided body" do
       body = "foo"
 
-      signature = validator.build_signature_for(url, body)
+      signature = validator.build_signature_for(body)
 
-      expect(signature).to(eq("HCAShLIZgdTK1ByO3LN7UywbjIQ="))
+      expect(signature).to(eq("QjZpKdTK/iwkUWiz3seoPbJHA0I="))
     end
 
     context "with nil params" do
       it "is valid" do
-        signature = validator.build_signature_for(nil, nil)
+        signature = validator.build_signature_for(nil)
         expect(signature).to(eq("KT7FsM8VSFUliCTsf6xdxj0XaRU="))
       end
     end
@@ -23,12 +22,12 @@ RSpec.describe Mergent::RequestValidator do
 
   describe "#valid_signature?" do
     it "returns true when the signature is valid" do
-      signature = validator.build_signature_for(nil, nil)
-      expect(validator.valid_signature?(nil, nil, signature)).to(eq(true))
+      signature = validator.build_signature_for(nil)
+      expect(validator.valid_signature?(nil, signature)).to(eq(true))
     end
 
     it "returns false when the signature is invalid" do
-      expect(validator.valid_signature?(nil, nil, "invalidsignature")).to(eq(false))
+      expect(validator.valid_signature?(nil, "invalidsignature")).to(eq(false))
     end
   end
 end
